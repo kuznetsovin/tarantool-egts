07eed034ff5a8f74a59b1d93467d36863b8054bb
Igor Kuznetsov <kuznetsovin@gmail.com>
2022-06-18 19:42:24 +0300

    feat: create response ack

--- egts/lib.c
+++ egts/lib.c
@@ -28,9 +28,15 @@ enum SubRecordType {
    EGTS_SR_POS_DATA = 16
 };
 
+enum RecordStatus {
+   EGTS_PC_OK = 0,
+   EGTS_PC_HEADERCRC_ERROR = 137,
+   EGTS_PC_DATACRC_ERROR = 138,
+};
+
 static struct fiber *f_egts_srv = NULL;
 
-uint16_t bytes_to_int16(unsigned char *first_byte)
+uint16_t bytes_to_uint16_le(unsigned char *first_byte)
 {
     uint16_t result = 0;
     result += (unsigned char) *first_byte << 8 * 0;
@@ -39,7 +45,7 @@ uint16_t bytes_to_int16(unsigned char *first_byte)
     return result;
 }
 
-uint32_t bytes_to_int32(unsigned char *first_byte)
+uint32_t bytes_to_uint32_le(unsigned char *first_byte)
 {
     uint32_t result = 0;
     result += (unsigned char) *first_byte << 8 * 0;
@@ -55,8 +61,13 @@ conn_handler(va_list ap)
 {
     int conn = va_arg(ap, int);
     unsigned char *buf = calloc(CONN_BUFFER_SIZE, sizeof(unsigned char));
+    unsigned char *response = calloc(CONN_BUFFER_SIZE, sizeof(unsigned char));
+    size_t resp_size = 0;
+    size_t oid = 0;
 
+    size_t result_code;
     while (true) {
+        result_code = EGTS_PC_OK;
         // read bytes for detection header egts packet len
         int rcv_count = recv(conn, buf, HEADER_MIN_LEN, 0);
         if (rcv_count == -1)
@@ -82,10 +93,10 @@ conn_handler(va_list ap)
             goto exit;
         }
 
-        uint16_t frame_data_len = bytes_to_int16(&buf[5]);
+        uint16_t frame_data_len = bytes_to_uint16_le(&buf[5]);
         say_info("frame data len: %zu", frame_data_len);
 
-        uint16_t pid = bytes_to_int16(&buf[7]);;
+        uint16_t pid = bytes_to_uint16_le(&buf[7]);;
         say_info("packet identifier: %zu", pid);
 
         size_t packet_type = buf[9];
@@ -103,7 +114,8 @@ conn_handler(va_list ap)
         if (fact_header_crc != header_crc)
         {
             say_error("invalid crc header: expected %X actual %X", header_crc, fact_header_crc);
-            continue;
+            result_code = EGTS_PC_HEADERCRC_ERROR;
+            goto response;
         }
 
         size_t current_packet_size = header_length + frame_data_len + DATA_CRC_SIZE;
@@ -120,25 +132,29 @@ conn_handler(va_list ap)
             goto exit;
         }
 
-        uint16_t frame_data_crc = bytes_to_int16(&buf[header_length+frame_data_len]);
+        uint16_t frame_data_crc = bytes_to_uint16_le(&buf[header_length+frame_data_len]);
         say_info("data frame crc: %zu", frame_data_crc);
 
         size_t fact_frame_data_crc = Crc16(&buf[header_length], frame_data_len);
         if (fact_frame_data_crc != frame_data_crc)
         {
             say_error("invalid crc data: expected %zu actual %zu", frame_data_crc, fact_frame_data_crc);
-            continue;
+            result_code = EGTS_PC_DATACRC_ERROR;
+            goto response;
         }
 
         size_t current_offset = header_length;
-        size_t oid = 0;
+
+        size_t sdr_responses_len = 0;
+        unsigned char *sdr_responses = NULL;
+
         while (current_offset < header_length+frame_data_len) {
-            uint16_t record_len = bytes_to_int16(&buf[current_offset]);
+            uint16_t record_len = bytes_to_uint16_le(&buf[current_offset]);
             say_info("record length: %zu", record_len);
 
             current_offset += 2;
 
-            uint16_t record_number = bytes_to_int16(&buf[current_offset]);;
+            uint16_t record_number = bytes_to_uint16_le(&buf[current_offset]);;
             say_info("record number: %zu", record_number);
             current_offset += 2;
 
@@ -147,7 +163,7 @@ conn_handler(va_list ap)
 
             if (rfl & 1) {
                 // parse oid
-                oid = bytes_to_int32(&buf[current_offset]);
+                oid = bytes_to_uint32_le(&buf[current_offset]);
 
                 current_offset += 4;
 
@@ -171,7 +187,7 @@ conn_handler(va_list ap)
             while (srd_offest < record_len) {
                 uint8_t subrecord_type = buf[srd_offest];
                 srd_offest += 1;
-                uint8_t subrecord_len = bytes_to_int16(&buf[srd_offest]);
+                uint8_t subrecord_len = bytes_to_uint16_le(&buf[srd_offest]);
                 srd_offest += 2;
 
                 switch (subrecord_type) {
@@ -182,19 +198,19 @@ conn_handler(va_list ap)
                         say_info("parsing EGTS_SR_POS_DATA section");
 
                         //NavigationTime
-						uint32_t navigate_time = bytes_to_int32(&buf[srd_offest]);
+						uint32_t navigate_time = bytes_to_uint32_le(&buf[srd_offest]);
                         // navigate time in egts has offest from 01.01.2010 00:00:00 UTC
 						navigate_time += 1262304000;
 
 
                         //Latitude
-						double latitude = (double)bytes_to_int32(&buf[srd_offest+4]) * 90 / 0xFFFFFFFF;
+						double latitude = (double)bytes_to_uint32_le(&buf[srd_offest+4]) * 90 / 0xFFFFFFFF;
 
                         //Longitude
-						double longitude = (double)bytes_to_int32(&buf[srd_offest+8]) * 180 / 0xFFFFFFFF;
+						double longitude = (double)bytes_to_uint32_le(&buf[srd_offest+8]) * 180 / 0xFFFFFFFF;
 
                         //Speed
-						uint16_t speed = bytes_to_int16(&buf[srd_offest+13]);
+						uint16_t speed = bytes_to_uint16_le(&buf[srd_offest+13]);
                         // first bit contains dir higest dir bit
                         uint8_t dir_higest_bit = speed >> 15;
 
@@ -222,13 +238,82 @@ conn_handler(va_list ap)
             }
 
             current_offset = srd_offest;
+
+            // create response record data section (see egts specification)
+            // every subrecord contains 5 bytes
+            sdr_responses = realloc(sdr_responses, sdr_responses_len + 6);
+
+            sdr_responses[sdr_responses_len] = EGTS_SR_RECORD_RESPONSE;
+            sdr_responses[sdr_responses_len+1] = 0x03;
+            sdr_responses[sdr_responses_len+2] = 0x00;
+            sdr_responses[sdr_responses_len+3] = record_number & 0xff;
+            sdr_responses[sdr_responses_len+4] = record_number >> 8;
+            sdr_responses[sdr_responses_len+5] = EGTS_PC_OK;
+
+            sdr_responses_len += 6;
+        }
+
+response:
+        resp_size = HEADER_MIN_LEN + DATA_CRC_SIZE;
+
+        size_t body_len = 3;
+        if (sdr_responses_len > 0) {
+            body_len += 7 + sdr_responses_len;
+        }
+
+        resp_size += body_len;
+        if (resp_size > CONN_BUFFER_SIZE)
+        {
+            response = realloc(response, resp_size);
+        }
+
+        // create response header
+        response[0] = 0x01;
+        response[1] = 0x00;
+        response[2] = 0x00;
+        response[3] = 0x0b;
+        response[4] = 0x00;
+        response[5] = body_len & 0xff;
+        response[6] = body_len >> 8;
+        response[7] = (pid+1) & 0xff;
+        response[8] = (pid+1) >> 8;
+        response[9] = EGTS_PT_RESPONSE;
+        response[10] = Crc8(response, HEADER_MIN_LEN - 1);
+
+        // create response body
+        response[11] = (pid) & 0xff;
+        response[12] = (pid) >> 8;
+        response[13] = result_code;
+
+        if (sdr_responses_len > 0) {
+            response[14] = sdr_responses_len & 0xff;
+            response[15] = sdr_responses_len >> 8;
+            response[16] = 0x01;
+            response[17] = 0x00;
+            response[18] = 0x00;
+            response[19] = 0x01;
+            response[20] = 0x01;
+            memcpy(&response[21], sdr_responses, sdr_responses_len);
+        }
+
+        // add finaly response crc
+        uint16_t resp_crc = Crc16(response, resp_size - DATA_CRC_SIZE);
+        response[resp_size - DATA_CRC_SIZE] = resp_crc & 0xff;
+        response[resp_size - DATA_CRC_SIZE + 1] = resp_crc >> 8;
+
+        if (send(conn, response, resp_size, 0) < 0)
+        {
+            say_error("send response error: %s", strerror(errno));
+            goto exit;
         }
 
         memset(buf, 0, CONN_BUFFER_SIZE);
-        break;
+        memset(response, 0, resp_size);
+        free(sdr_responses);
     }
-   // TODO: send response packet
 exit:
+    free(buf);
+    free(response);
     fiber_cancel(fiber_self());
     close(conn);
 
